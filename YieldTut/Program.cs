b650dc5965e0e329c4ae878bc3dc52cee42d132c
tanjilulanwar <tanjilulanwar@gmail.com>
2022-07-20 14:26:07 +0600

    new

--- YieldTut/Program.cs
+++ YieldTut/Program.cs
@@ -1,22 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using BenchmarkDotNet.Analysers;
-using BenchmarkDotNet.Configs;
-using BenchmarkDotNet.Exporters;
-using BenchmarkDotNet.Loggers;
-using BenchmarkDotNet.Running;
 namespace YieldTut
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // ProcessPeople();
-           
-            var summary = BenchmarkRunner.Run<YieldBenchmarks>();
-        
+             ProcessPeople();
+          //  ProcessPeopleYield();
+        }
 
+
+        public static void ProcessPeople()
+        {
+            var people = GetPeople(1000000);
+            foreach (var person in people)
+            {
+                if (person.Id < 1000000)
+                    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}");
+                else
+                    break;
+            }
+        }
+
+        static IEnumerable<Person> GetPeople(int count)
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < count; i++)
+            {
+                people.Add(new Person() { Id = i, Name = $"Name {i}" });
+            }
+            return people;
+        }
+
+   
+        public static void ProcessPeopleYield()
+        {
+            var people = GetPeopleYield(1000000);
+            foreach (var person in people)
+            {
+                if (person.Id < 1000000)
+                    Console.WriteLine($"Id: {person.Id}, Name: {person.Name}");
+                else
+                    break;
+            }
+        }
+
+        static IEnumerable<Person> GetPeopleYield(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return new Person() { Id = i, Name = $"Name {i}" };
+            }
+        }
+
+        public IEnumerable<int> MyProperty
+        {
+            get
+            {
+                yield return 0;
+                yield return 1;
+                yield return 2;
+                yield return 3;
+                yield return 4;
+            }
         }
     }
 }
